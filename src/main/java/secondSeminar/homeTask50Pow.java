0e6f7183d30e6397427e98a580fbd3c919b25436
Lily <ms.lilichka@yandex.ru>
2022-10-28 12:45:20 +0500

    homeTasks seminar Two

--- src/main/java/secondSeminar/homeTask50Pow.java
+++ src/main/java/secondSeminar/homeTask50Pow.java
@@ -12,7 +12,6 @@ public class homeTask50Pow {
 }
 
 
-
 //package secondSeminar;
 //
 //public class homeTask50Pow {
--- src/main/java/secondSeminar/homeTask70Ladder.java
+++ src/main/java/secondSeminar/homeTask70Ladder.java
@@ -4,13 +4,14 @@ public class homeTask70Ladder {
     public static void main(String[] args) {
 
     }
+
     public int climbStairs(int n) {
         int[] arr = new int[46]; //1 <= n <= 45 по усл зд
         arr[0] = 1;
         arr[1] = 1;
         arr[2] = 2;
-        for(int i=3; i<=n; i++){
-            arr[i] = arr[i-1] + arr[i-2];
+        for (int i = 3; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
         }
         return arr[n];
     }
